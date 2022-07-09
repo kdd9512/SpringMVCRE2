a0522172db7909e8f881be950700ee01348a27d9
kdd9512 <hyunwoo8805@gmail.com>
2022-07-09 23:41:47 +0900

    20220709 3rd commit
    BoardMapper.xml 로 쿼리 분리(기존 query annotation 은 주석처리)
    
    간단한 create(insert) test 추가. 정상동작.
    bno 가 필요한 경우 / 필요없는 경우를 분리하여 호출 가능하게끔 query 를 2개 생성.

--- src/main/java/org/kdd9512/mapper/BoardMapper.java
+++ src/main/java/org/kdd9512/mapper/BoardMapper.java
@@ -6,8 +6,12 @@ import org.kdd9512.domain.BoardVO;
 import java.util.List;
 
 public interface BoardMapper {
-
-    @Select("select * from tbl_board where bno > 0")
+    // resources 의 xml 파일로 대체하였으므로 주석처리.
+    // @Select("select * from tbl_board where bno > 0")
     public List<BoardVO> getList();
+
+    public void insert(BoardVO board);
+
+    public void insertSelectKey(BoardVO board);
 
 }
--- src/test/java/org/kdd9512/mapper/BoardMapperTests.java
+++ src/test/java/org/kdd9512/mapper/BoardMapperTests.java
@@ -4,6 +4,7 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kdd9512.domain.BoardVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -17,9 +18,34 @@ public class BoardMapperTests {
     private BoardMapper mapper;
 
     @Test
-    public void testGetList(){
+    public void testGetList() {
 
         mapper.getList().forEach(log::info);
+
+    }
+
+    @Test
+    public void testInsert() {
+        BoardVO board = new BoardVO();
+
+        board.setTitle("새글");
+        board.setContent("새 내용");
+        board.setWriter("늅늅늅");
+
+        mapper.insert(board);
+        log.info(board);
+    }
+
+    @Test
+    public void testInsertSelectKey(){
+        BoardVO board = new BoardVO();
+
+        board.setTitle("새글 select key");
+        board.setContent("새 내용 select key");
+        board.setWriter("늅늅늅");
+
+        mapper.insertSelectKey(board);
+        log.info(board);
 
     }
 
